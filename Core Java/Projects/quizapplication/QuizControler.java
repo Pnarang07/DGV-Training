8e1cb036e842e44c8ef839ca1fba5e7ffb4de37d
Pnarang07 <99171772+Pnarang07@users.noreply.github.com>
2022-10-26 23:34:13 +0530

    Update QuizControler.java

--- Core Java/Projects/quizapplication/QuizControler.java
+++ Core Java/Projects/quizapplication/QuizControler.java
@@ -8,247 +8,56 @@ public class QuizControler {
 	
 	Scanner scanner=new Scanner(System.in);
 	
-//	Quiz quiz=new Quiz();
-	
+	ArrayList<QuizPlayer> playerData=new ArrayList<>();
 	ArrayList<Quiz> quizData=new ArrayList<>();
 	
 	int choice;
 	int count;
-//	String name; 
-	
+	int answer=0;
 	public void quizArray(){
 		
-		quizData.add(new Quiz("Sarvesh",65));
-		quizData.add(new Quiz("Devansh",55));
-		quizData.add(new Quiz("Sujit",30));
-		quizData.add(new Quiz("Ayush",15));
-		quizData.add(new Quiz("Ramesh",90));
+		playerData.add(new QuizPlayer("Sarvesh",65));
+		playerData.add(new QuizPlayer("Devansh",55));
+		playerData.add(new QuizPlayer("Sujit",30));
+		playerData.add(new QuizPlayer("Ayush",15));
+		playerData.add(new QuizPlayer("Ramesh",90));
 	}
 	public void playGame(String Name) {
+		
+	quizData.add(new Quiz("What is the capital of India?","1.Mumbai","2.Delhi",2));
+	quizData.add(new Quiz("What is Virat's surname","1.Kholi","2.Sharma",1));
+	quizData.add(new Quiz("What is the favourite sport of all indians","1.Football","2.Cricket",2));
+	quizData.add(new Quiz("What is 12 * 12","1.144","2.136",1));
+	quizData.add(new Quiz("What is Parth's Favourite Sport","1.Cricket","2.Badminton",2));
+	quizData.add(new Quiz("What is 50 + 50","1.100","2.250",1));
+	quizData.add(new Quiz("Where is Virat's HomeTown","1.Delhi","2.Lucknow",1));
+	quizData.add(new Quiz("Which Laptop does DGV Tech gives us","1.Dell","2.HP",2));
+	quizData.add(new Quiz("What is Full Form of DGV","1.DigiVridhi Technologies","2.DedonaArramSe",1));
+	quizData.add(new Quiz("Who is Ashneer Grover","1.Founder of Paytm","2.Founder of BharatPe",2));
 	
-			count=0;
-//		System.out.println("Please enter your name");
-//		name=scanner.nextLine();
-
+	for(int i=0;i<10;i++) {
+		System.out.println(quizData.get(i).getQuestion());
+		System.out.println(quizData.get(i).getOption1());
+		System.out.println(quizData.get(i).getOption2());
+		answer=scanner.nextInt();
 		
-		
-		System.out.println("Does Parth lives in Navi Mumbai");
-		System.out.println("1. True 2. False");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
+		if(answer==quizData.get(i).getAnswer()) {
+		count+=10;
+		System.out.println("Score : "+count);
 	}
-		
-		System.out.println("Is Parth a Software Engineer");
-		System.out.println("1. True 2. False");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		System.out.println("Does Parth works in DGV Technologies");
-		System.out.println("1. True 2. False 3.Don't Know");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 3:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		
-		System.out.println("Where is Parth's HomeTown");
-		System.out.println("1. Shahabad Markanda 2. Navi Mumbai 3. Lucknow 4. Rajpura");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 3:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 4:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		
-		System.out.println("Who hit the last shot in the match of India Vs Pakistan?");
-		System.out.println("1. Virat 2. Ashwin 3. Pandya");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-			
-		}
-		case 2:{
-			
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-			
-		}
-		case 3:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-			
-		}
-	}
-		System.out.println("What is the capital of India");
-		System.out.println("1. Delhi 2. Mumbai 3. Kolkata 4. Banglore");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 3:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 4:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		System.out.println("Parth's surname is Narang?");
-		System.out.println("1. True 2. False");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		System.out.println("Is Parth a big Foddieee?");
-		System.out.println("1. True 2. False");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		System.out.println("Is Parth a loyal person?");
-		System.out.println("1. True 2. False");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		System.out.println("Parth's Favourite sport is Badminton?");
-		System.out.println("1. True 2. False");
-		
-		choice=scanner.nextInt();
-		
-		switch(choice) {
-		case 1:{
-			count+=10;
-			System.out.println("Score: " + count);
-			break;
-		}
-		case 2:{
-			count-=5;
-			System.out.println("Score: " + count);
-			break;
-		}
-	}
-		quizData.add(new Quiz(Name,count));
-		
-	}
-	
+	else {
+		count-=5;
+		System.out.println("Score : "+count);
+	}	
+}
+	playerData.add(new QuizPlayer(Name,count));
+}	
 	public void seeRankings() {
 		
-		Collections.sort(quizData,new topFive());
+		Collections.sort(playerData,new topFive());
 		
 		for(int i=0;i<5;i++) {
-			System.out.println(quizData.get(i).getName()+" "+quizData.get(i).getScore());
+			System.out.println(playerData.get(i).getName()+" "+playerData.get(i).getScore());
 		}
 		
 	}
